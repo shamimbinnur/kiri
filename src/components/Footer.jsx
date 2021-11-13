0f4a3ce6f07222cae5e0fb3a1503a8a4fcccf7cd
shamimbinnur <sbnsbn00@gmail.com>
2021-11-13 15:04:33 +0600

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,28 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className=" bg-kiri-blue py-8">
-        <div className="container flex flex-col md:flex-row items-center">
-            <div className="flex flex-1 flex-wrap items-center justify-center">
-                <ul className="flex text-white uppercase gap-12 text-xs">
-                    <li className="cursor-pointer "> <a href="#home">home</a> </li>
-                    <li className="cursor-pointer "> <a href="#feature">feature</a> </li>
-                </ul>
+        <footer className=" bg-kiri-blue py-3 fixed bottom-0 left-0 w-full z-50  ">
+            <div className="container flex flex-col md:flex-row items-center">
+                <div className="flex flex-1 flex-col flex-wrap  items-center justify-center">
+                    <h1 className=" text-sm text-gray-100 mb-2 font-medium ">Community</h1>
+                    <ul className="flex text-white flex-col text-center md:text-left text-xs">
+                        <li className="cursor-pointer "> <a href="">Top.gg Website</a> </li>
+                        <li className="cursor-pointer "> <a href="#feature">Discord Support Server</a> </li>
+                        <li className="cursor-pointer "> <a href="#feature">Discord Community Server</a> </li>
+                    </ul>
+                </div>
+
+                <div className="flex flex-1 flex-col flex-wrap items-center justify-center mt-2 md:mt-0 ">
+                    <h1 className=" text-sm text-gray-100 mb-2 font-medium ">More</h1>
+                    <ul className="flex text-white flex-col text-xs text-center md:text-left">
+                        <li className="cursor-pointer "> <a href="">Github</a> </li>
+                    </ul>
+                </div>
+
+                
             </div>
-        </div>
-    </footer>
+            <p className="text-sm text-gray-50 mt-2 text-center"> C 2021 Kiri</p>
+        </footer>
     )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,19 @@ import Kiri from "../images/kiri.jpeg"
 function Hero() {
     return (
         <section className=" bg-hero-pattern bg-no-repeat bg-center ">
-            <div className="container flex justify-center p-6">
+            <div className="container mx-auto p-10 md:py-24  ">
                 {/* Photo & Branding */}
-                <div>
-                    <div className=" h-64 w-64  rounded-full overflow-hidden border-4 border-kiri-1 mt-4 mb-8 p-0">
-                        <img className="w-full h-full object-cover" src={Kiri} alt="kiri" />
+                    <div className="flex flex-col justify-center  items-center mb-10">
+                        <h1 className="text-8xl text-center text-kiri-2 font-medium mb-1 " >Kiri</h1>
+                        <h1 className="text-center text-xl  text-gray-700">"I'm Kiri, an assistant that provides all around entertainment and fun to your server with games, memes and economy (o^▽^o)" i think</h1>
                     </div>
-                    <h1 className="text-8xl text-center text-kiri-2 font-medium mb-1 " >Kiri</h1>
-                    <div className=" text-xl text-kiri-blue ">
-                        <h1 className="text-center">Lorem Ipsum</h1>
+                    
+                    <div className=" flex flex-col items-center justify-center ">
+                        <div className=" h-64 w-64  rounded-full overflow-hidden border-4 border-kiri-1 mt-4 mb-8 p-0">
+                            <img className="w-full h-full object-cover" src={Kiri} alt="kiri" />
+                        </div>  
+                        <button className="w-44 h-14 mt-5 hover:bg-gray-100 duration-500 ease-in-out hover:text-kiri-blue hover:border-gray-900 transition-all  rounded-full border-2 border-dashed border-gray-100 bg-kiri-blue font-bold text-gray-200 text-lg "> <a href=""> ADD TO DISCORD </a></button>   
                     </div>
-                </div>
             </div>          
         </section>
     )
