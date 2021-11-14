b43f23b1f6c3be5499aa0097451257949c6d0447
shamimbinnur <sbnsbn00@gmail.com>
2021-11-14 23:17:15 +0600

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ function Footer() {
     return (
         <footer className=" bg-gray-100 z-50 fixed bottom-0 left-0 w-full py-8 border-t-2 border-color-gray-100 ">
             <div className="container flex flex-col gap-x-3.5 gap-y-3 justify-center md:flex-row  items-center">
-                <div className="w-40 h-10 text-md  bg-gray-300 ring-2 ring-gray-400 text-gray-700 hover:text-gray-50 hover:bg-gray-700 duration-500 ease-in-out rounded-full flex justify-center items-center gap-x-1.5">
+                <div className="w-40 h-10 text-md  bg-gray-300 ring-2 ring-gray-400 text-gray-600 hover:text-gray-50 hover:bg-gray-600 duration-500 ease-in-out rounded-full flex justify-center items-center gap-x-1.5">
                     <i class="  fab fa-discord"></i>
                     <button className="font-medium" > <a href=""> Add to Discord </a></button>
                 </div>
 
-                <div className="w-40 h-10 text-md  bg-gray-300 ring-2 ring-gray-400 text-gray-700 hover:text-gray-50 hover:bg-gray-700 duration-500 ease-in-out rounded-full flex justify-center items-center gap-x-1.5">
+                <div className="w-40 h-10 text-md  bg-gray-300 ring-2 ring-gray-400 text-gray-600 hover:text-gray-50 hover:bg-gray-600 duration-500 ease-in-out rounded-full flex justify-center items-center gap-x-1.5">
                     <i class="fas fa-book-open"></i>
                     <button className="font-medium" > <a href=""> Read the guide </a></button>
                 </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,16 +5,16 @@ function Nav() {
     return (
         <header>
             <nav className=" flex items-center p-4  fixed z-50 w-full top-0 shadow-lg bg-gray-100">
-                <ul className="flex flex-1   justify-center items-center gap-6 md:gap-12  text-kiri-blue uppercase text-sm md:text-md font-medium     ">
-                    <div className="flex justify-center items-center gap-x-1.5 hover:text-blue-500  " >
+                <ul className="flex flex-1   justify-center items-center gap-6 md:gap-12  text-gray-600 uppercase text-sm md:text-md font-medium     ">
+                    <div className="flex justify-center items-center gap-x-1.5 hover:text-gray-800  " >
                         <i class="fas fa-home"></i>
                         <li className="cursor-pointer duration-300 ease-in-out "> <a href="#home">home</a> </li>
                     </div>
-                    <div className="flex justify-center items-center gap-x-1.5 hover:text-blue-500 " >
+                    <div className="flex justify-center items-center gap-x-1.5 hover:text-gray-800 " >
                         <i class="fas fa-bahai"></i>
                         <li className="cursor-pointer duration-300 ease-in-out "> <a href="#features">features</a> </li> 
                     </div>
-                    <div className="flex justify-center items-center gap-x-1.5 hover:text-blue-500 " >
+                    <div className="flex justify-center items-center gap-x-1.5 hover:text-gray-800 " >
                         <i class="fas fa-hand-holding-heart"></i>
                         <li className="cursor-pointer duration-300 ease-in-out "> <a href="#donation">Donation</a> </li>
                     </div>
