4613c5891496c0ec3024f3228da431e5e11511e9
Shamim Bin Nur <48399373+shamimbinnur@users.noreply.github.com>
2021-11-23 23:46:07 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ function Footer() {
             <div className="container flex flex-col gap-x-3.5 gap-y-3 justify-center md:flex-row  items-center">
                 <div className="w-40 h-10 text-md  bg-gray-300 ring-2 ring-gray-400 text-gray-600 hover:text-gray-50 hover:bg-gray-600 duration-500 ease-in-out rounded-full flex justify-center items-center gap-x-1.5">
                     <i class="  fab fa-discord"></i>
-                    <a href="https://kiribot.xyz/"> <button className="font-medium" > Add to Discord </button> </a>
+                    <a href="/"> <button className="font-medium" > Add to Discord </button> </a>
                 </div>
 
                 <div className="w-40 h-10 text-md  bg-gray-300 ring-2 ring-gray-400 text-gray-600 hover:text-gray-50 hover:bg-gray-600 duration-500 ease-in-out rounded-full flex justify-center items-center gap-x-1.5">
                     <i class="fas fa-book-open"></i>
-                    <a href="https://kiribot.xyz/"> <button className="font-medium" > Read the guide </button> </a>
+                    <a href=""> <button className="font-medium" > Read the guide </button> </a>
                 </div>
             </div>
         </footer>
